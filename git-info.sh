655dbdb1d1b0fc2a0dc471111f2bc25ec6afcaa8
Markus Pesch <markus.pesch@agfa.com>
2019-05-21 11:13:56 +0200

    fix: git-info

--- git-info.sh
+++ git-info.sh
@@ -3,11 +3,8 @@
 if [ $1 == "date" ]; then
   git log -1 --pretty=format:"%ad" --date=short
 elif [ $1 == "commit" ]; then
-  if [[ -z ${VERSION+x} || ${#VERSION} -le 0 ]]; then
-    git rev-parse --short HEAD
-  else
-    echo ${VERSION}
-  fi
+  GV=$(git rev-parse --short HEAD)-git
+  echo ${VERSION:-$GV}
 elif [ $1 == "url" ]; then
   URL=$(git config --get remote.origin.url | sed -e 's#^git@#https://#' -e 's#systems:#systems/#' -e 's#_#\\_#g' -e 's#.git$##' )
   if [ -z "$URL" ]; then
